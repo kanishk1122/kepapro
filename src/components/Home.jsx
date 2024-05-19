18b68ad7b7b5614c60c392c7330bbb1cb34f2517
kanishk1123 <apimails1@gmail.com>
2024-05-19 07:10:56 +0000

    sdjcd

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -323,7 +323,7 @@ const Home = () => {
                             alt=""
                           />
                         </div>
-                        <div className="w-full h-1/3 text-center pt-3 text-[3vw] font-semibold ">
+                        <div className="w-full h-1/3 text-center pt-3 text-[3%] font-semibold ">
                           {item.animename}
                         </div>
                       </div>
