87dc94cbf02732eb616076744b139df0c0e9577b
kanishk1123 <apimails1@gmail.com>
2024-05-19 16:28:04 +0000

    acsas

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,23 +10,26 @@ import { Link, NavLink } from "react-router-dom";
 import "../assets/public/css/navbar.css";
 import { detailsContext } from "../utils/Context";
 
+import Cookies from 'js-cookie';
+
 function getUserFromToken() {
-  const token = localStorage.getItem("token");
+  const token = Cookies.get('token');
   if (!token) return null;
 
-  const base64Url = token.split(".")[1];
-  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
   const jsonPayload = decodeURIComponent(
     atob(base64)
-      .split("")
+      .split('')
       .map(function (c) {
-        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
       })
-      .join("")
+      .join('')
   );
 
   return JSON.parse(jsonPayload);
 }
+
 
 const Navbar = ({ setsearchResult, resultsearch }) => {
   const checkinguser = useMemo(() => getUserFromToken(), []);
