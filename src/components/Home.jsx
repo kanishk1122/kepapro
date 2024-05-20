881dad8db53a96507bf6b9591a584a035ed09734
kanishk1123 <apimails1@gmail.com>
2024-05-20 18:33:42 +0000

    acs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,24 +23,7 @@ const Home = () => {
 
   console.log(Cookies.get("token"))
 
-  function jwt_decode (token) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
 
-    return JSON.parse(jsonPayload);
-}
-
-const decodingToken = () => {
-  try {
-    const decoded = jwt_decode(token); // Decode the token
-    setDecodedToken(decoded); // Update the decodedToken state with the decoded token
-  } catch (error) {
-    console.error("Error decoding token:", error); // Log any errors that occur during decoding
-  }
-};
 
 
   const divstyle = {
