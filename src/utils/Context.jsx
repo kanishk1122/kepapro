e0bd90129bcc581f45357b51b67a5736315d7eed
kanishk1123 <apimails1@gmail.com>
2024-05-20 11:17:16 +0000

    amin

--- src/utils/Context.jsx
+++ src/utils/Context.jsx
@@ -1,16 +1,48 @@
 import { useState, useEffect, createContext } from 'react';
 import axios from './Axios';
-
+import Cookies from 'js-cookie';
 
 export const detailsContext = createContext();
 
 const Context = (props) => {
     const [data, setData] = useState([]);
-    const [gettoken,settoken] = useState("")
+    const [gettoken,settoken] = useState("");
+    const[userdata,setuserdata]= useState({})
    // Renamed to follow conventions
+
+
+   const [token, setToken] = useState(Cookies.get("token")); // State to hold the JWT string
+   const [decodedToken, setDecodedToken] = useState(""); 
+ 
+   function jwt_decode (token) {
+     var base64Url = token.split('.')[1];
+     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+     var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+     }).join(''));
+ 
+ 
+     return JSON.parse(jsonPayload);
+ 
+     
+ }
+ 
+ 
+ 
+   // Function to decode the token
+   const decodingToken = () => {
+     try {
+       const decoded = jwt_decode(token); // Decode the token
+       setDecodedToken(decoded); // Update the decodedToken state with the decoded token
+     } catch (error) {
+       console.error("Error decoding token:", error); // Log any errors that occur during decoding
+     }
+     console.log(jwt_decode(token));
+   };
 
     useEffect(() => {
         console.log("Component mounted");
+        decodingToken()
         
         const fetchData = async () => {
             try {
@@ -20,9 +52,10 @@ const Context = (props) => {
                 console.error("Error fetching data:", error);
                 // Handle error if needed
             }
+        
         };
-    
        fetchData() // Fetch data every 5 seconds
+      
     
        
     }, [setData]); // Add setData as a dependency
