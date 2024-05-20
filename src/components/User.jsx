82a9768e9cdb6f9b5c31e3c54d915f794fc55171
kanishk1123 <apimails1@gmail.com>
2024-05-20 20:04:41 +0000

    acs

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -66,13 +66,14 @@ const getbookmarkshower = ()=>{
 
     <>
     <Navbar/>
-      {jwt_decode(token).email == username ? ( <div className='bg-neutral-900 w-full h-fit text-white'>
+     <div className=''>
+     {jwt_decode(token).email == username ? ( <div className='bg-neutral-900 w-full h-fit text-white'>
         <div className=' min-h-[50vh] justify-center gap-10 items-center h-fit flex flex-wrap relative w-[100vw]'>
             <div className='w-[200px] min-w-[200px] min-h-[200px]   h-[200px] bg-zinc-700 rounded-full'></div>
-            <div className='text-4xl tracking-widest w-[300px] h-[300px]  flex flex-col gap-3  px-8 py-3 rounded-2xl '>
+            <div className='text-4xl tracking-widest w-[300px] h-full text-center  flex flex-col gap-3  px-8 py-3 rounded-2xl '>
                 <h1>kansihk@321</h1>
                
-                <h1>kansih</h1>
+                <h2>kansih</h2>
                
                 
                
@@ -83,7 +84,7 @@ const getbookmarkshower = ()=>{
             <div className='w-120px h-120px'>
             <svg xmlns="http://www.w3.org/2000/svg" onClick={getbookmarkshower} className={` ${showbookmark ? "w-[5vw] h-[5vw]": "w-[10vw] h-[10vw]" } min-h-[70px] min-w-[70px]  duration-700`} viewBox="0 0 24 24" fill="currentColor"><path d="M4 2H20C20.5523 2 21 2.44772 21 3V22.2763C21 22.5525 20.7761 22.7764 20.5 22.7764C20.4298 22.7764 20.3604 22.7615 20.2963 22.7329L12 19.0313L3.70373 22.7329C3.45155 22.8455 3.15591 22.7322 3.04339 22.4801C3.01478 22.4159 3 22.3465 3 22.2763V3C3 2.44772 3.44772 2 4 2ZM19 19.9645V4H5V19.9645L12 16.8412L19 19.9645ZM12 13.5L9.06107 15.0451L9.62236 11.7725L7.24472 9.45492L10.5305 8.97746L12 6L13.4695 8.97746L16.7553 9.45492L14.3776 11.7725L14.9389 15.0451L12 13.5Z"></path></svg>
             </div>
-            <div className='text-4xl  font-semibold'>
+            <div onClick={getbookmarkshower}  className='text-4xl  font-semibold'>
               {showbookmark ?  <p>Bookmarks</p> : <div>click to show bookmarks</div>  }
               
             </div></div>
@@ -97,6 +98,7 @@ const getbookmarkshower = ()=>{
       somethinbg went wrong
     </div>) }
     
+     </div>
     <Footer/>
     </>
   )
