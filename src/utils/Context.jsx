ccc7559c8028ba759b8900de9e76095e4fbdb974
kanishk1123 <apimails1@gmail.com>
2024-05-20 11:25:04 +0000

    asc

--- src/utils/Context.jsx
+++ src/utils/Context.jsx
@@ -15,14 +15,19 @@ const Context = (props) => {
    const [decodedToken, setDecodedToken] = useState(""); 
  
    function jwt_decode (token) {
-     var base64Url = token.split('.')[1];
-     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-     var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-     }).join(''));
- 
- 
-     return JSON.parse(jsonPayload);
+   if(token){
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+
+    return JSON.parse(jsonPayload);
+   }
+   else{
+    console.log("token is not avalebvle")
+   }
  
      
  }
