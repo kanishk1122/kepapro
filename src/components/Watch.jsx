05d0bcff84195a4f1c8b95f5b80563efd7760ec2
kanishk1123 <apimails1@gmail.com>
2024-05-21 08:16:33 +0000

    ypcbas

--- src/components/Watch.jsx
+++ src/components/Watch.jsx
@@ -28,7 +28,7 @@ const Watch = () => {
 
   
 
-if (token) {
+if (Cookies.get("token")) {
   function jwt_decode (token) {
     var base64Url = token.split('.')[1];
     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
