bc497690360a91de00b4a22a110ea87bf1280eef
kanishk1123 <apimails1@gmail.com>
2024-05-21 14:15:55 +0000

    asc

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -66,7 +66,7 @@ const User = () => {
       }
     };
     fetchcontent();
-    userdata.bookmarks.map(1)
+    userdata.bookmarks.animename.map(1)
   }, [userdata.email]);
 
   // useEffect(() => {
