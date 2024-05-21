03d22099badeb8438d925263531b98e99f6ff3e9
kanishk1123 <apimails1@gmail.com>
2024-05-21 12:17:06 +0000

    asc

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -67,7 +67,6 @@ const User = () => {
     <>
     
       <Navbar />
-      <form onLoad={handleSubmit}></form>
       <div className=''>
         <div className='w-full flex justify-end items-end '>
           <svg onClick={userLogout} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36" fill="currentColor">
