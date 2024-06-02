b4b1cdb1de1d45c5b879fe25326a44e7975cd869
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2024-06-02 17:40:00 +0530

    asvds

--- src/utils/Routing.jsx
+++ src/utils/Routing.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from "../components/Home.jsx";
 import Watch from '../components/Watch.jsx';
-import Allanime from "../components/Allanime.jsx';
-import User from "../components/User.jsx';
-import News from "../components/News.jsx';
-import Registration from "../components/Registration.jsx';
+import Allanime from '../components/Allanime.jsx';
+import User from '../components/User.jsx';
+import News from '../components/News.jsx';
+import Registration from '../components/Registration.jsx';
 import Login from '../components/Login.jsx';
 import Adminlogin from '../components/Adminlogin.jsx';
 import Adminreg from '../components/Adminreg.jsx';
@@ -20,20 +20,27 @@ const Routing = () => {
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/watch/:name/:seo/:episode' element={<Watch />} />
+      <Route path='/watch/:name/:seo/:episode/register' element={<Registration />} />
+      <Route path='/watch/:name/:seo/:episode/login' element={<Login />} />
+      <Route path='/watch/:name/:seo/:episode' element={<Watch />} />
       <Route path='/all/trending' element={<Allanime />} />
+      <Route path='/all/trending/register' element={<Registration />} />
+      <Route path='/all/trending/login' element={<Login />} />
       <Route path='/all/popular' element={<Allpopular />} />
+      <Route path='/all/popular/register' element={<Registration />} />
+      <Route path='/all/popular/login' element={<Login />} />
       <Route path='/user/:username' element={<User />} />
       <Route path='/edit/:username' element={<Edituser />} />
       <Route path='/news' element={<News />} />
-      <Route path='/*/register' element={<Registration />} />
-      <Route path='/*/login' element={<Login />} />
+      <Route path='/register' element={<Registration />} />
+      <Route path='/login' element={<Login />} />
       <Route path='/adminlogin' element={<Adminlogin />} />
       <Route path='/adminreg' element={<Adminreg />} />
       <Route path='/upload' element={<Upload />} />
       <Route path='/ads.txt' element={<Ads />} />
       <Route path='/decode' element={<DecodedToken />} />
       {/* Add a fallback route for unknown paths */}
-      <Route path='*' element={<Navigate to='/' />} />
+      {/* <Route path='*' element={<Navigate to='/' />} /> */}
     </Routes>
   );
 }
