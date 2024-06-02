30e6251570c0dcafb53a7049d2182ac499435b43
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2024-06-02 17:21:45 +0530

    asc

--- src/utils/Routing.jsx
+++ src/utils/Routing.jsx
@@ -1,48 +1,41 @@
-import React from 'react'
-import { Routes, Route, Navigate, Link } from 'react-router-dom'
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from "../components/Home.jsx";
 import Watch from '../components/Watch.jsx';
-import Allanime from "../components/Allanime.jsx";
-import User from "../components/User.jsx";
-import News from "../components/News.jsx";
-import Registration from "../components/Registration.jsx";
+import Allanime from "../components/Allanime.jsx';
+import User from "../components/User.jsx';
+import News from "../components/News.jsx';
+import Registration from "../components/Registration.jsx';
 import Login from '../components/Login.jsx';
 import Adminlogin from '../components/Adminlogin.jsx';
 import Adminreg from '../components/Adminreg.jsx';
 import Upload from '../components/Upload.jsx';
-import Ads from "../components/ads.txt"
-import Allpopular from "../components/Allpopular.jsx"
+import Ads from "../components/ads.txt";
+import Allpopular from "../components/Allpopular.jsx";
 import DecodedToken from '../components/DecodeToken.jsx';
 import Edituser from '../components/Edituser.jsx';
 
 const Routing = () => {
-
   return (
-    
-  
-        <Routes>
-          <Route path='/' element={<Home />} exact  />
-          <Route path='/watch/:name/:seo/:episode' element={<Watch />} />
-          <Route path='/all/trending' element={<Allanime/>} />
-          <Route path='/all/popular' element={<Allpopular/>} />
-          <Route path='/user/:username' element={<User />} />
-          <Route path='/edit/:username' element={<Edituser />} />
-          <Route path='/news' element={<News />} />
-          <Route path='/*/register' element={<Registration />} />
-          <Route path='/*/login' element={<Login />} />
-          <Route path='/adminlogin' element={<Adminlogin />} />
-          <Route path='/adminreg' element={<Adminreg />} />
-          <Route path='/upload' element={<Upload />} />
-          <Route path='/ads.txt' element={<Ads/>} />
-          <Route path='/decode' element={<DecodedToken />} />
-
-          {/* Add more routes as needed */}
-          {/* Redirect to 404 page for unknown routes */}
-        </Routes>
-   
-    
-    
-  )
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/watch/:name/:seo/:episode' element={<Watch />} />
+      <Route path='/all/trending' element={<Allanime />} />
+      <Route path='/all/popular' element={<Allpopular />} />
+      <Route path='/user/:username' element={<User />} />
+      <Route path='/edit/:username' element={<Edituser />} />
+      <Route path='/news' element={<News />} />
+      <Route path='/*/register' element={<Registration />} />
+      <Route path='/*/login' element={<Login />} />
+      <Route path='/adminlogin' element={<Adminlogin />} />
+      <Route path='/adminreg' element={<Adminreg />} />
+      <Route path='/upload' element={<Upload />} />
+      <Route path='/ads.txt' element={<Ads />} />
+      <Route path='/decode' element={<DecodedToken />} />
+      {/* Add a fallback route for unknown paths */}
+      <Route path='*' element={<Navigate to='/' />} />
+    </Routes>
+  );
 }
 
-export default Routing
+export default Routing;
